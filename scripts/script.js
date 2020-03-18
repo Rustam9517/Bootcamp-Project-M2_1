06d55dba52c8bacd5d2607e46cf80f1a962e3ab3
Rustam9517 <61832301+Rustam9517@users.noreply.github.com>
2020-03-18 17:27:56 +0400

    First version

---
+++ scripts/script.js
@@ -0,0 +1,41 @@
+let checkButton = document.querySelector('button');
+let error = document.querySelector('.error');
+
+checkButton.addEventListener('click',function () {
+    // Math operations Start
+    let startSum = +(document.querySelector('#startSum').value);
+    let monthlyAdd = +(document.querySelector('#monthlyAdd').value);
+    let annualPercent = +(document.querySelector('#annualPercent').value);
+    let days = +(document.querySelector('#days').value);
+    let month = Math.floor(days/30);
+    let result= (startSum*Math.pow(1+(annualPercent/100)/12,month)) + monthlyAdd*pow(month,annualPercent);
+                //Вычисление ежемесячной суммы                           //Вычисление ежемесячного взноса и его увеличивание
+
+    function pow(time,annual){//Цикл меняющий ещемесячный взнос в зависимости от количества месяцев
+        let power = 0;
+        for(let i=time;i>0;i--){
+            power += Math.pow(1+(annual/100)/12,i);
+        }
+        return power;
+    }
+    // Math operations End
+
+
+    // Error notification start
+    if(startSum<0){
+        error.innerHTML = `Начальная сумма вклада Должна быть положительным числом!!!!`;
+    }else
+    if(monthlyAdd<0){
+        error.innerHTML = `Сумма ежемесячного пополнения Должна быть положительным числом!!!!`;
+    }else
+    if(annualPercent<0 || annualPercent>100){
+        error.innerHTML = `Процентная ставка Должна быть положительным числом меньше 100!!!!`;
+    }else
+    if(days<0 || Number.isInteger(days)===false){
+        error.innerHTML = `Срок Должен быть положительным целым числом!!!!`;
+    }
+    else{
+        alert(result.toFixed(2));
+    }
+    // Error notification end
+});
